9bf9fee94b9e1a7db297319b8aee2fd8a94838dc
offSimonEsders <simonesders2003@gmail.com>
2024-03-16 00:11:26 +0100

    better responsivnes

--- src/app/user-access/user-access.component.ts
+++ src/app/user-access/user-access.component.ts
@@ -1,14 +1,33 @@
-import { NgStyle } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-access',
   standalone: true,
-  imports: [RouterOutlet, NgStyle],
+  imports: [RouterOutlet, NgStyle, NgClass],
   templateUrl: './user-access.component.html',
   styleUrl: './user-access.component.scss'
 })
 export class UserAccessComponent {
+  @ViewChild('accessframe') accessframe?: ElementRef;
+  @ViewChild('header') header?: ElementRef;
+  centerContent: boolean = false;
 
+  switchClass(element: HTMLElement, removeClass: string, addClass: string) {
+    setTimeout(() => {
+      element.classList.remove(removeClass);
+      element.classList.add(addClass)
+    }, 2000);
+  }
+
+  @HostListener('document:DOMContentLoaded', ['$event'])
+  @HostListener('window: resize', ['$event'])
+  checkHeight() {
+    if (this.accessframe?.nativeElement.offsetHeight <= window.outerHeight) {
+      this.centerContent = true;
+    } else {
+      this.centerContent = false;
+    }
+  }
 }
