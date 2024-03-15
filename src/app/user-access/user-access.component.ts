0198d095ae5fda11e0acc8f2675d5c9ba28224b5
offSimonEsders <simonesders2003@gmail.com>
2024-03-15 19:56:18 +0100

    some logo animations

--- src/app/user-access/user-access.component.ts
+++ src/app/user-access/user-access.component.ts
@@ -1,10 +1,11 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-access',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgStyle],
   templateUrl: './user-access.component.html',
   styleUrl: './user-access.component.scss'
 })
