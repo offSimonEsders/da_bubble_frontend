a37f086099ee061d41fde75dd6268452919d9370
offSimonEsders <simonesders2003@gmail.com>
2024-03-16 01:17:03 +0100

    register active checkbox

--- src/app/user-access/register/register.component.ts
+++ src/app/user-access/register/register.component.ts
@@ -1,13 +1,18 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  checkBox: FormControl = new FormControl('', [Validators.requiredTrue]);
 
+  constructor (public router: Router) {
+  }
 }
