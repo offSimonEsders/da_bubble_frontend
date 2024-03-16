23d06b396374e9de1842ead62245b52797d1a38c
offSimonEsders <simonesders2003@gmail.com>
2024-03-16 15:22:35 +0100

    load img

--- src/app/user-access/choose-avatar/choose-avatar.component.ts
+++ src/app/user-access/choose-avatar/choose-avatar.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +11,19 @@ import { Router } from '@angular/router';
 })
 export class ChooseAvatarComponent {
   @Input() name?: string;
+  previewImage: string = '../../../assets/avatars/anonymus.svg';
 
   constructor(public router: Router) {
+  }
+
+  setImage(path: string) {
+    this.previewImage = path
+  }
+
+  loadImage(value: any) {
+    if(value) {
+      const img = new File([value[0]], this.name + '_avatar');
+      this.previewImage = URL.createObjectURL(img);
+    }
   }
 }
