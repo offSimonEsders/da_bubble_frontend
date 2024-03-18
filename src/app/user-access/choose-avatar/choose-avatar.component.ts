6c102b6f827a2d853083d0e66e3baab73936df2f
offSimonEsders <simonesders2003@gmail.com>
2024-03-18 21:48:31 +0100

    discovered that django imagefield does not accept svg

--- src/app/user-access/choose-avatar/choose-avatar.component.ts
+++ src/app/user-access/choose-avatar/choose-avatar.component.ts
@@ -15,35 +15,48 @@ export class ChooseAvatarComponent {
   @Input() username?: string;
   @Input() email?: string;
   @Input() password?: string;
-  previewImage: string = '../../../assets/avatars/anonymus.svg';
+  previewImage: string = '../../../assets/avatars/anonymus.png';
+  customAvatar?: Blob = undefined;
+  choosenAvatar?: string = undefined;
 
   constructor(public router: Router, private backendservice: BackendService) {
   }
 
   setImage(path: string) {
     this.previewImage = path;
+    this.choosenAvatar = path;
+    this.customAvatar = undefined;
   }
 
-  loadImage(value: any) {
-    if(value) {
-      const avatar: any = new File([value[0]], this.username + '_avatar');
-      this.previewImage = URL.createObjectURL(avatar);
-      this.createFormdata(value[0]);
+  loadImage(value: FileList | null) {
+    if (value) {
+      this.previewImage = URL.createObjectURL(value[0] as Blob);
+      this.customAvatar = value[0];
     }
   }
-  
+
+  async validateAvatarAndContinueWithRegistration() {
+    if (this.customAvatar) {
+      this.createFormdata(this.customAvatar);
+    } else if (this.choosenAvatar) {
+      const resp = await fetch(this.choosenAvatar, { method: 'GET' });
+      this.createFormdata(await resp.blob());
+    }
+  }
+
   createFormdata(avatar: Blob) {
-    if(this.username && this.email && this.password) {
-      let formData = new FormData();
+    if (this.username && this.email && this.password) {
+      const file = new File([avatar], 'avatar.png', { type: 'image/png' });
+      const formData = new FormData();
       formData.append('username', this.username);
       formData.append('email', this.email);
       formData.append('password', this.password);
-      formData.append('avatar', avatar);
-      this.registerNewUser(formData);
+      formData.append('avatar', file);
+      this.callBackendServiceAndvalidateResponse(formData);
     }
   }
 
-  async registerNewUser(formData: FormData) {
+  async callBackendServiceAndvalidateResponse(formData: FormData) {
     const resp = await this.backendservice.registerNewUser(formData);
   }
 
